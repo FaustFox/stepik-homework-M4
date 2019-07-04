e9deeff841d425fe5572d7d908ff774ddf573bd7
FaustFox <faustffox@gmail.com>
2019-07-04 03:31:33 +0300

    add basket page test from product page

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import BasketPageLocators
 
+
 class BasketPage(BasePage):
 
     def basket_should_have_product(self):
@@ -11,11 +12,10 @@ class BasketPage(BasePage):
         goods = self.is_not_element_present(*BasketPageLocators.GOODS_IN_BASKET)
         assert goods, "Basket should be empty"
 
-    def basket_without_product_should_have_message_of_emptyness(self):
-        message = self.is_element_present(*BasketPageLocators.BASKET_EMPTY)
-        assert message, "Empty basket should have message that its empty"
-
     def basket_with_product_should_not_have_message_of_emptyness(self):
         message = self.is_not_element_present(*BasketPageLocators.BASKET_EMPTY)
         assert message, "Basket with goods should not have message of empty basket"
-        
+
+    def basket_without_product_should_have_message_of_emptyness(self):
+        message = self.is_element_present(*BasketPageLocators.BASKET_EMPTY)
+        assert message, "Empty basket should have message that its empty"
--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,8 @@
 import pytest
 
+from pages.cart_page import BasketPage
 from pages.product_page import ProductPage
 
-
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 def test_guest_can_go_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -15,17 +10,31 @@ def test_guest_can_go_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
-def test_should_see_button_add_to_basket(browser):
-    page = ProductPage(browser, ProductPage.link)
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
     page.open()
-    page.should_be_button_add_to_basket()
-
-def test_could_add_product_with_button_add_to_basket(browser):
-    page = ProductPage(browser, ProductPage.link)
-    page.open()
-    page.click_button_add_to_basket()
-
+    page.go_to_basket_page()
+    page.basket_should_not_have_product()
+    page.basket_without_product_should_have_message_of_emptyness()
+    
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, ProductPage.link)
     page.open()
     page.should_not_be_success_message()
+
+def test_guest_could_add_product_with_button_add_to_basket(browser):
+    page = ProductPage(browser, ProductPage.link)
+    page.open()
+    page.click_button_add_to_basket()
+
+def test_guest_should_see_button_add_to_basket(browser):
+    page = ProductPage(browser, ProductPage.link)
+    page.open()
+    page.should_be_button_add_to_basket()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
