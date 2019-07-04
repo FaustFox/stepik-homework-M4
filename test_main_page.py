f95ed38390381548fd3a5523c09bc9aae96f5319
FaustFox <faustffox@gmail.com>
2019-07-04 05:42:40 +0300

    add negative basket page test from main and product page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.cart_page import BasketPage
+from pages.product_page import ProductPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -10,6 +11,17 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+def test_guest_cant_see_empty_message_in_basket_from_main_page(browser):
+    page = ProductPage(browser, ProductPage.link)
+    page.open()
+    page.click_button_add_to_basket()
+    link = "http://selenium1py.pythonanywhere.com"
+    basket_page = BasketPage(browser, link)
+    basket_page.open()
+    basket_page.go_to_basket_page()
+    basket_page.basket_should_have_product()
+    basket_page.basket_with_product_should_not_have_message_of_emptyness()
 
 def test_guest_cant_see_product_in_cart_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
--- test_product_page.py
+++ test_product_page.py
@@ -10,6 +10,15 @@ def test_guest_can_go_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
+def test_guest_cant_see_empty_message_in_basket_from_product_page(browser):
+    page = ProductPage(browser, ProductPage.link)
+    page.open()
+    page.click_button_add_to_basket()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_should_have_product()
+    basket_page.basket_with_product_should_not_have_message_of_emptyness()
+
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
